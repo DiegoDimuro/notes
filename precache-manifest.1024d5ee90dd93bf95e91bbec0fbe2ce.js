313988667aa82f88c505cd95b45b2a5b8e9ef9a6
Diego Andres Dimuro <dimurodiego@gmail.com>
2022-08-16 15:30:30 -0300

    Updates

---
+++ precache-manifest.1024d5ee90dd93bf95e91bbec0fbe2ce.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6246cb54d7df67427554cac85842c4ed",
+    "url": "/notes/index.html"
+  },
+  {
+    "revision": "cbf33254bb275da9a79c",
+    "url": "/notes/static/js/2.08d39621.chunk.js"
+  },
+  {
+    "revision": "d3cb2f3d1689a0e8facb",
+    "url": "/notes/static/js/main.2ca8dca9.chunk.js"
+  },
+  {
+    "revision": "7fd48c41dd37627d6393",
+    "url": "/notes/static/js/runtime-main.de3b9034.js"
+  }
+]);
