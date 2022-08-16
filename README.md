cebc4e5dec09c10f2b2bfd8b410e9fef93a1faab
Diego Andres Dimuro <dimurodiego@gmail.com>
2022-08-16 15:01:00 -0300

    base code upload

---
+++ README.md
@@ -0,0 +1,19 @@
+## Tech Stack-
+- HTML
+- CSS
+- React Js (JSX)
+- Material UI
+
+* Run this command to install all dependencies for the project.
+```
+npm install
+```
+<br/>
+
+* Run this command to start the project on local host 3000.
+```
+node src/index.js or npm start
+```
+<br/>
+
+* Now you are all set to use this project.
---
+++ build/precache-manifest.fefb22ae7d258e4c77687b61397fa556.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "708a2f7d0465913c6543aed7b1f44f4c",
+    "url": "/notes/index.html"
+  },
+  {
+    "revision": "cbf33254bb275da9a79c",
+    "url": "/notes/static/js/2.08d39621.chunk.js"
+  },
+  {
+    "revision": "57fbf1bdf052073d8e9f",
+    "url": "/notes/static/js/main.86782e02.chunk.js"
+  },
+  {
+    "revision": "7fd48c41dd37627d6393",
+    "url": "/notes/static/js/runtime-main.de3b9034.js"
+  }
+]);
